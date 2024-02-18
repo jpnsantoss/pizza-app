69ed6ff2eced8ae7ce47e2b5c98ebe8e2ee768c5
jpnsantoss <jpnsantos14@gmail.com>
2024-02-18 23:41:12 +0000

    user_repository + authentication_bloc

---
+++ packages/user_repository/lib/src/entities/user_entity.dart
@@ -0,0 +1,31 @@
+class MyUserEntity {
+  final String userId;
+  final String email;
+  final String name;
+  final bool hasActiveCart;
+
+  MyUserEntity({
+    required this.userId,
+    required this.email,
+    required this.name,
+    required this.hasActiveCart,
+  });
+
+  Map<String, Object> toDocument() {
+    return {
+      'userId': userId,
+      'email': email,
+      'name': name,
+      'hasActiveCart': hasActiveCart,
+    };
+  }
+
+  static MyUserEntity fromDocument(Map<String, dynamic> doc) {
+    return MyUserEntity(
+      userId: doc['userId'],
+      email: doc['email'],
+      name: doc['name'],
+      hasActiveCart: doc['hasActiveCart'],
+    );
+  }
+}
